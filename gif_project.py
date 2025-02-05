e2ad23be59c4780b6bac57278ca19a7934c1f85a
mr-chris-reed <chrree@bhasd.org>
2025-02-05 08:58:24 -0500

    cropping, speed changes added

--- gif_project.py
+++ gif_project.py
@@ -6,7 +6,7 @@ clip = clip.resized(clip.size[::-1])
 
 clip = clip.resized(0.2)
 
-cropped_clip = clip.crop(x1=50, y1=50, x2=50, y2=250)
+clip = clip.cropped(x1=0, y1=35, x2=200, y2=250)
 
 text1 = TextClip(
     font="Doto-Black.ttf",
@@ -34,5 +34,7 @@ text1 = text1.with_position((int(video_size[0]/2 - text1_size[0]/2), int(video_s
 text2 = text2.with_position((int(video_size[0]/2 - text2_size[0]/2), int(video_size[1]/10 + text1_size[1] + text1_size[1]/2))).with_duration(2)
 
 output = CompositeVideoClip([clip, text1, text2])
+
+output = output.with_speed_scaled(2)
 
 output.write_gif('output.gif')
