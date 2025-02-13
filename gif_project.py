3c05659827d1134d9490e27c7cb47578a6916885
mr-chris-reed <chrree@bhasd.org>
2025-02-13 07:28:06 -0500

    new gif, replaced old one

--- gif_project.py
+++ gif_project.py
@@ -1,6 +1,6 @@
 from moviepy import *
 
-clip = VideoFileClip("rolling.MOV")
+clip = VideoFileClip("moxie.MP4")
 
 clip = clip.resized(clip.size[::-1])
 
@@ -9,31 +9,21 @@ clip = clip.resized(0.2)
 clip = clip.cropped(x1=0, y1=35, x2=200, y2=250)
 
 text1 = TextClip(
-    font="Doto-Black.ttf",
-    text="Rolling,",
-    font_size=30,
-    color=(0, 0, 0),
-)
-
-text2 = TextClip(
-    font="Doto-Black.ttf",
-    text="Rolling!!!",
+    font="FontdinerSwanky-Regular.ttf",
+    text="Moxie!",
     font_size=30,
     color=(0, 0, 0),
 )
 
 video_size = clip.size
 text1_size = text1.size
-text2_size = text2.size
 
 print(video_size)
 print(text1_size)
-print(text2_size)
 
 text1 = text1.with_position((int(video_size[0]/2 - text1_size[0]/2), int(video_size[1]/10))).with_duration(2)
-text2 = text2.with_position((int(video_size[0]/2 - text2_size[0]/2), int(video_size[1]/10 + text1_size[1] + text1_size[1]/2))).with_duration(2)
 
-output = CompositeVideoClip([clip, text1, text2])
+output = CompositeVideoClip([clip, text1])
 
 output = output.with_speed_scaled(2)
 
